d0f878ea68843f7fff37ca2591d78f07a073e168
Johann Iván Olarte Arboleda <johann.olarte@gmail.com>
2019-07-02 16:56:34 -0500

    Added player's movements tests

---
+++ GoD.UnitTests/Core/Games/GameServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using GoD.WebApi.Core.Games;
+using GoD.WebApi.Core.Models;
+using GoD.WebApi.Core.ViewModels;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace GoD.UnitTests.Core.Games
+{
+    [TestFixture]
+    public class GameServiceTests
+    {
+        [Test]
+        public void ValidateMoves_PlayerOnePaperMoveAndPlayerTwoScissorsMove_ReturnPlayerTwoName()
+        {
+            // Arrange
+            var playersMoves = new List<MovesViewModel>
+            {
+                new MovesViewModel
+                {
+                    Move = "Paper",
+                    Player = new Player
+                    {
+                        Id = 17,
+                        Name = "Victor",
+                        Active = true
+                    }
+                },
+                new MovesViewModel
+                {
+                    Move = "Scissors",
+                    Player = new Player
+                    {
+                        Id = 18,
+                        Name = "Michael",
+                        Active = true
+                    }
+                }
+            };
+
+            // Act
+            var result = GameService.ValidateMoves(playersMoves);
+
+            // Result
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<string>());
+            Assert.That(result, Is.EqualTo(playersMoves[1].Player.Name));
+        }
+    }
+}
