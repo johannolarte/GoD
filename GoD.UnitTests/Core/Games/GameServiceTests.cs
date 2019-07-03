4ebdfca0d0702d7bc18863d0d5f5342c5a37b27a
Johann Iván Olarte Arboleda <johann.olarte@gmail.com>
2019-07-03 14:44:16 -0500

    Update GameServiceTests.cs
    Added all movements validations

--- GoD.UnitTests/Core/Games/GameServiceTests.cs
+++ GoD.UnitTests/Core/Games/GameServiceTests.cs
@@ -9,15 +9,16 @@ namespace GoD.UnitTests.Core.Games
     [TestFixture]
     public class GameServiceTests
     {
-        [Test]
-        public void ValidateMoves_PlayerOnePaperMoveAndPlayerTwoScissorsMove_ReturnPlayerTwoName()
+        private List<MovesViewModel> _playersMoves;
+
+        [SetUp]
+        public void SetUp()
         {
-            // Arrange
-            var playersMoves = new List<MovesViewModel>
+            _playersMoves = new List<MovesViewModel>
             {
                 new MovesViewModel
                 {
-                    Move = "Paper",
+                    Move = string.Empty,
                     Player = new Player
                     {
                         Id = 17,
@@ -27,7 +28,7 @@ namespace GoD.UnitTests.Core.Games
                 },
                 new MovesViewModel
                 {
-                    Move = "Scissors",
+                    Move = string.Empty,
                     Player = new Player
                     {
                         Id = 18,
@@ -36,14 +37,58 @@ namespace GoD.UnitTests.Core.Games
                     }
                 }
             };
+        }
+
+        [TestCase("Paper", "Rock")]
+        [TestCase("Rock", "Scissors")]
+        [TestCase("Scissors", "Paper")]
+        public void ValidateMoves_PlayerOneWinnerMoveAndPlayerTwoFailMove_ReturnPlayerOneName(string playerOneMove, string playerTwoMove)
+        {
+            // Arrange
+            _playersMoves[0].Move = playerOneMove;
+            _playersMoves[1].Move = playerTwoMove;
 
             // Act
-            var result = GameService.ValidateMoves(playersMoves);
+            var result = GameService.ValidateMoves(_playersMoves);
 
             // Result
             Assert.That(result, Is.Not.Null);
             Assert.That(result, Is.TypeOf<string>());
-            Assert.That(result, Is.EqualTo(playersMoves[1].Player.Name));
+            Assert.That(result, Is.EqualTo(_playersMoves[0].Player.Name));
+        }
+
+        [TestCase("Paper", "Scissors")]
+        [TestCase("Rock", "Paper")]
+        [TestCase("Scissors", "Rock")]
+        public void ValidateMoves_PlayerOneFailMoveAndPlayerTwoWinnerMove_ReturnPlayerTwoName(string playerOneMove, string playerTwoMove)
+        {
+            // Arrange
+            _playersMoves[0].Move = playerOneMove;
+            _playersMoves[1].Move = playerTwoMove;
+
+            // Act
+            var result = GameService.ValidateMoves(_playersMoves);
+
+            // Result
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<string>());
+            Assert.That(result, Is.EqualTo(_playersMoves[1].Player.Name));
+        }
+
+        [TestCase]
+        public void ValidateMoves_PlayerOneAndPlayerTwoRockMove_ReturnDrawMessage()
+        {
+            // Arrange
+            _playersMoves[0].Move = "Rock";
+            _playersMoves[1].Move = "Rock";
+
+            // Act
+            var result = GameService.ValidateMoves(_playersMoves);
+
+            // Result
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<string>());
+            Assert.That(result, Is.EqualTo("Draw"));
         }
     }
 }
